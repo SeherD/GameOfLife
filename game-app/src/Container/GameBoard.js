786f473a3f45644f0ee978450e2ac26741588684
Taylor Noel <taylor.noel1@outlook.com>
2023-11-11 10:51:35 -0700

    add spinner to board

--- game-app/src/Container/GameBoard.js
+++ game-app/src/Container/GameBoard.js
@@ -1,28 +1,45 @@
 import React, {Component} from 'react';
 import Tile from '../Components/Tile'
+import WheelComponent from 'react-wheel-of-prizes'
+//import 'react-wheel-of-prizes/dist/index.css'
 
 
+const segments = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const segColors = ['#EE4040',
+'#F0CF50',
+'#815CD1',
+'#3DA5E0',
+'#34A24F',
+'#F9AA1F',
+'#EC3F3F',
+'#FF9000',
+'#EE4040',
+'#F0CF50',
+];
 export default class GameBoard extends Component{
     state = {
-        emptyPoints: [208, 204, 193, 178, 177, 162, 161, 174, 144, 130, 131, 132, 119, 104, 59, 74, 
-        88,87, 71, 56, 26, 14, 12, 9, 8, 7, 6, 20, 35, 2, 17, 16, 15, 48, 47, 45, 60, 75, 78, 79, 81,
+        emptyPoints: [209, 204, 194, 179, 178, 177, 176, 174, 145, 146, 147, 134, 104, 
+        118, 117, 115, 99, 84, 85, 88, 24, 39, 8, 7, 20, 35, 2, 3, 16, 15, 48, 47, 45, 60, 75, 78, 79, 81,
         96, 97, 112, 126, 125, 123, 122, 157, 155, 154, 153, 169, 183, 182, 213, 214, 200],
-        careerPoints: [44, 13, 49, 124],
-        paydayPoints: [189, 159, 133, 29, 10, 4, 50, 30, 76, 142, 156, 137, 197],
-        stopPoints: [160, 89, 5, 127, 184],
-        startPoints: [223, 219],
+        careerPoints: [89, 87, 49, 124],
+        paydayPoints: [189, 159, 148, 86, 54, 4, 50, 30, 76, 142, 156, 137, 197],
+        stopPoints: [175, 119, 5, 127, 184],
+        startPoints: [224, 219],
         endPoints: [185],
-        housePoints: [134, 11, 80, 152],
-        skillPoints: [129, 86, 41, 3, 46, 77, 212, 215]
+        housePoints: [149, 69, 6, 80, 152],
+        skillPoints: [144, 116, 114, 9, 17, 46, 77, 212, 215]
     }
+
+    onFinished = (winner) => {
+        console.log(winner)
+      }  
 
     createBoard = () =>{
 
         return Array.from({ length: 15 }).map((_, rowIndex) => 
-        <div style={{ display: 'flex' }}>{
+        <div className ='boardRow'>{
           Array.from({ length: 15 }).map((_, colIndex) => {
             var num = rowIndex*15 + colIndex;
-            console.log(num);
                 if(this.state.careerPoints.includes(num)){
                     return <Tile
                                 number = {num}
@@ -80,8 +97,18 @@ export default class GameBoard extends Component{
     
 
     render() {return (
-    <div>
+    <div className = 'board'>
         {this.createBoard()}
+        <div className = 'spinner'>
+        <WheelComponent
+        segments = {segments}
+        segColors ={segColors}
+        onFinished = {(winner) => this.onFinished(winner)}
+        isOnlyOnce = {false}
+        downDuration ={500}
+        />
+        </div>
+        
     </div>
   );}
 }
