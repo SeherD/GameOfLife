5e21642f47c6043a7a638f0c278142632593ee5d
Taylor Noel <taylor.noel1@outlook.com>
2023-11-23 15:20:18 -0700

    add basic landing page and react router

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './Pages/LandingPage';
 import GamePage from './Pages/GamePage';
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-      <GamePage/>
-      </header>
+    <header className="App-header">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element = {<LandingPage/>}/>
+      <Route path="game" element ={<GamePage/>}/>
+    </Routes>
+    </BrowserRouter>
+    </header>
     </div>
   );
 }
