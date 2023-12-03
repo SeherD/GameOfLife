b58d4b1fbd3b880e5bfb87b8c7e9f43f30c1b833
Seher Dawar <seherdawar@gmail.com>
2023-12-03 16:35:19 -0700

    reconnect functionality (maybe bug in player id)

--- flask-app/app.py
+++ flask-app/app.py
@@ -14,7 +14,13 @@ CORS(app)
 api = Api(app)
 socketio = SocketIO(app, cors_allowed_origins="*")  # Adjust the origins as needed
 
-# Your existing endpoint registrations go here (e.g., api.add_resource(SomeResource, '/some_endpoint'))
+
+def load_all_player_data():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM Players")
+    all_player_data = cursor.fetchall()
+    return all_player_data
 
 connected_players = set()
 
@@ -25,9 +31,42 @@ def handle_connect():
         connected_players.add(request.sid)
         join_room(request.sid)
         print(f"Player {request.sid} connected. Total connected players: {len(connected_players)}")
+
+        # Load all player data from the database
+        all_player_data = load_all_player_data()
+
+        # Send all player data to the new player
+        emit('all_player_data', all_player_data, room=request.sid)
+
+        # Broadcast to all clients about the new player
+        emit('new_player_connected', {'player_id': request.sid}, broadcast=True)
+
     else:
         print("Room full")
 
+def load_player_data(player_id):
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM Players WHERE PlayerID=?", (player_id,))
+    player_data = cursor.fetchone()
+
+    return player_data   
+  
+@socketio.on('reconnect')
+def handle_reconnect():
+    # Get the player ID from the request
+    player_id = request.sid
+
+    # Load the player data from the database
+    player_data = load_player_data(player_id)
+
+    if player_data:
+        # Send the player data to the reconnected player
+        emit('load_player_data', player_data, room=request.sid)
+        print(f"Player {player_id} reconnected.")
+
+    
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
