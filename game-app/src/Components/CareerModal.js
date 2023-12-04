33c91abe23a198e0fc48c99ec46868f48ca306f0
Taylor Noel <taylor.noel1@outlook.com>
2023-12-03 23:45:26 -0700

    update career images

--- game-app/src/Components/CareerModal.js
+++ game-app/src/Components/CareerModal.js
@@ -1,61 +1,69 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import webdev from '../assets/webdev.png';
 import CareerCard from './CareerCard.js';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import axios from 'axios';
+import { CareerImage } from './services/CareerImage.js';
 
 export default class CareerModal extends Component {
     constructor(props) {
         super(props);
         this.state = {
             currentSlide: 0,
-            currentCareerId: 'C1',
-            careerOptions: []
+            careerOptions: [],
+            careerCards: []
         };
     }
 
     componentDidMount() {
         axios({
             method: "GET",
-            url:"http://localhost:5000/career/filterByUniversity/P" + (this.props.playerIndex + 1),
-          })
-          .then((response) => {
-            this.setState({careerOptions: response.data.available_career_cards});
-
-          })
+            url: "http://localhost:5000/career/filterByUniversity/P" + (this.props.playerIndex + 1),
+        })
+        .then((response) => {
+            this.setState({ careerOptions: response.data.available_career_cards });
+        });
     }
-  
+
+    componentDidUpdate(prevProps, prevState) {
+        // Check if career options have changed
+        if (prevState.careerOptions !== this.state.careerOptions) {
+            // Populate career cards and update state
+            this.populateModal().then(careerCards => {
+                this.setState({ careerCards });
+            });
+        }
+    }
+
     handleSlideChange = (index) => {
-        this.setState({ currentSlide: index, currentCareerId: this.state.careerOptions[index].CareerID });
+        this.setState({ currentSlide: index });
     };
 
     handleClose = () => {
         const { handleClose, onModalClose } = this.props;
-        // Use the callback to send the current slide index and current career to GameBoard.js
         if (onModalClose) {
-            onModalClose(this.state.currentSlide, this.state.currentCareerId);
+            onModalClose(this.state.currentSlide, this.state.careerOptions[this.state.currentSlide].CareerID);
         }
-        // Close the modal
         handleClose();
     }
 
-
-    populateModal = () =>{
-        return this.state.careerOptions.map((career) =>(
-            <CareerCard key = {career.CareerID} name = {career.Name} image = {webdev} salary = {career.Salary} />
-        ))
+    populateModal = async () => {
+        const cards = await Promise.all(this.state.careerOptions.map(async (career) => {
+            const img = CareerImage.GetImage(career.Image);
+            return <CareerCard key={career.CareerID} name={career.Name} image={img} salary={career.Salary} />;
+        }));
+        return cards;
     }
-  
+
     render() {
         return (
             <div>
-            <h1>Choose a career</h1>
+                <h1>Choose a career</h1>
                 <Carousel className="modal" onChange={this.handleSlideChange} onSlideChange={this.props.onSlideChange} showThumbs={false} showArrows={true}>
-                   {this.populateModal()}
+                    {this.state.careerCards}
                 </Carousel>
                 <button onClick={this.handleClose}>Select</button>
             </div>
-          );
+        );
     }
 }
