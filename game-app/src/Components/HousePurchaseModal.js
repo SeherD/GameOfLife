8865985651ead0d1c8a4770452c12a055bc13a0f
Taylor Noel <taylor.noel1@outlook.com>
2023-12-03 23:44:05 -0700

    update house images

--- game-app/src/Components/HousePurchaseModal.js
+++ game-app/src/Components/HousePurchaseModal.js
@@ -6,6 +6,7 @@ import nohouse from '../assets/nohouse.png';
 import HouseCard from './HouseCard.js';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import axios from 'axios';
+import { HouseImage } from './services/HouseImage.js';
 
 export default class HousePurchaseModal extends Component {
     houses = ['House','Apartment'];
@@ -13,8 +14,8 @@ export default class HousePurchaseModal extends Component {
         super(props);
         this.state = {
             currentSlide: 0,
-            currentHouseId: 'H1',
-            houseOptions: []
+            houseOptions: [],
+            houseCards: []
         };
     }
   
@@ -28,33 +29,45 @@ export default class HousePurchaseModal extends Component {
 
           })
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        // Check if house options have changed
+        if (prevState.houseOptions !== this.state.houseOptions) {
+            // Populate career cards and update state
+            this.populateHouses().then(houseCards => {
+                this.setState({ houseCards });
+            });
+        }
+    }
+
     handleSlideChange = (index) => {
-        this.setState({ currentSlide: index, currentHouse: this.state.houseOptions[index].HouseID });
+        this.setState({ currentSlide: index}, console.log("changing to " + this.state.houseOptions[index].HouseID ));
     };
 
     handleClose = () => {
         const { handleClose, onModalClose } = this.props;
         // Use the callback to send the current slide index and current house to GameBoard.js
         if (onModalClose) {
-            onModalClose(this.state.currentSlide, this.state.currentHouseId);
+            onModalClose(this.state.currentSlide, this.state.houseOptions[this.state.currentSlide].HouseID);
         }
         // Close the modal
         handleClose();
     }
 
-    populateHouses = () =>{
-        return this.state.houseOptions.map((house)=>(
-            <HouseCard key = {house.HouseID} name = {house.Name} image = {apartment} price = {house.Cost}/> 
-        ))
+    populateHouses = async () =>{
+        const cards = await Promise.all(this.state.houseOptions.map(async (house) => {
+            const img = HouseImage.GetImage(house.Image);
+            return <HouseCard key = {house.HouseID} name = {house.Name} image = {img} price = {house.Cost}/>; 
+    }));
+        return cards;
     }
   
     render() {
         return (
             <div>
             <h1>Choose a house to buy</h1>
-                {/* TODO: call a flask endpoint to display all the houses not owned by any player */}
                 <Carousel className="modal" onChange={this.handleSlideChange} onSlideChange={this.props.onSlideChange} showThumbs={false} showArrows={true}>
-                    {this.populateHouses()}
+                    {this.state.houseCards}
                 </Carousel>
                 <button onClick={this.handleClose}>Select</button>
             </div>
