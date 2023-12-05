86d1ff549e5cb77ec94ca16feec40283a724d289
allison-grothman <83380106+allison-grothman@users.noreply.github.com>
2023-12-05 15:21:33 -0700

    Implement endgame modal and score calculation
    Selling houses doesn't work because it doesn't wait for the player to spin.

--- game-app/src/Components/GameBoard.js
+++ game-app/src/Components/GameBoard.js
@@ -47,6 +47,10 @@ export default class GameBoard extends Component{
             playerPieces: [],
             // for tracking the initial path choice modal
             universityModalOpen: true,
+            // for tracking the final modal
+            finalCash: 0,
+            endgameModalOpen: false,
+
             players: [],
             currentPlayer: null
         }
@@ -291,31 +295,69 @@ export default class GameBoard extends Component{
         }
     };
 
+    handleRetirement = async () => {
+        // get a list of the player's houses
+        const response = await axios({
+            method: "GET",
+            url: "/players/houses/P" + (this.state.playerIndex + 1),
+        });
+        const houses = response.data.houses;
+
+        // prompt the player to sell all their houses
+        for (let i = 0; i < houses.length; i++) {
+            const house = houses[i];
+
+            toast(`Spin to see whether the price of ${house.Name} went up or down!`, {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "dark",
+                bodyClassName: "popup"
+            });
+
+            this.setState({ houseToSell: house, houseSpin: true });
+        }
+
+        // call a flask endpoint to calculate the player's total cash including skill bonuses
+        const skillPaymentsResponse = await axios({
+            method: "GET",
+            url: "/players/skill-payments/P" + (this.state.playerIndex + 1),
+        });
+
+        const totalCash = skillPaymentsResponse.data.cash;
+
+        this.setState({ finalCash: totalCash }, resolve);
+
+        // display the player's final cash
+        this.setState({ endgameModalOpen: true });
+    };
+
     handleTile = (onPath, atPosition) => {
         const index = this.state.path[onPath][atPosition];
         const tile = this.tiles[index];
-        const newValue = tile.handleClick();
-        // if handleClick returned a value
-        if (newValue) {
-            // if that value is a string, it is a new skill
-            if (newValue instanceof String) {
-            // if the returned value is a number, it is 2 * the player's salary
-            } else {
-                // call a flask endpoint to add newValue to the player's cash
-                axios({
-                    method: "PUT",
-                    url:"http://localhost:5000/players/payday/P" + (this.state.playerIndex + 1),
-                    data: {
-                        "double_earning": true
-                      }
-                  })
-                  .then((response) => {
-                  console.log('PUT request successful:', response.data);
-                  this.setState({currentPlayer: response.data})
-                  this.props.updatePlayerInfo(this.state.playerIndex + 1)
-
-                  })
-            }
+        tile.handleClick();
+        // if the tile is a payday point
+        if (this.state.paydayPoints.includes(index)) {
+            // call a flask endpoint to add double to the player's cash
+            axios({
+                method: "PUT",
+                url:"http://localhost:5000/players/payday/P" + (this.state.playerIndex + 1),
+                data: {
+                    "double_earning": true
+                    }
+                })
+                .then((response) => {
+                console.log('PUT request successful:', response.data);
+                this.setState({currentPlayer: response.data})
+                this.props.updatePlayerInfo(this.state.playerIndex + 1)
+            })
+        // if the tile is the retirement tile
+        } else if (this.state.endPoints.includes(index)) {
+            this.handleRetirement();
         }
     }
 
@@ -678,6 +720,19 @@ export default class GameBoard extends Component{
                     shouldCloseOnOverlayClick={false}
                     style={customStyles}>
                     <ModalContent playerIndex={this.state.playerIndex} type={"Career"} handleClose={() => this.setState({initialCareerModalOpen: false})} onModalClose={this.handleInitialCareerModalClose} />
+                </Modal>
+            </div>
+            {/* modal for displaying player's final score at endgame */}
+            <div>
+                <Modal
+                    ariaHideApp={false}
+                    isOpen = {this.state.endgameModalOpen}
+                    onRequestClose={() => this.setState({endgameModalOpen: true})}
+                    shouldCloseOnEsc={false}
+                    shouldCloseOnOverlayClick={false}
+                    style={customStyles}>
+                    <h1>YOU RETIRED!</h1>
+                    <p>You have ${this.state.finalCash}</p>
                 </Modal>
             </div>
         </div>
